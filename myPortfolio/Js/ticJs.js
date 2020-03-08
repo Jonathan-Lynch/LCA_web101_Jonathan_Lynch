3a5bbef4a639e05f8651618a6f3d697fe326c8ef
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-03-07 22:42:35 -0600

    Check Poit 2

---
+++ myPortfolio/Js/ticJs.js
@@ -0,0 +1,7 @@
+var gameMarker = "x"
+var gameMarker = "o"
+
+function changeMarkerToX(){console.log("The x button was clicked!") }
+function changeMarkerToO(){console.log("The o button was clicked!") }
+
+function placeMark(){ }
